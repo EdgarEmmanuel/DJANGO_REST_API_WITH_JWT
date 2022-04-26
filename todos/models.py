ec6dd083ac3625db50e86eee48f395c4e9aa1dff
Edgar <mmnledgar@gmail.com>
2022-04-26 23:03:49 +0000

    working on the REST API

--- todos/models.py
+++ todos/models.py
@@ -9,8 +9,14 @@ class User(models.Model):
     email=models.CharField(max_length=40)
     password=models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Todo(models.Model):
     title=models.CharField(max_length=200)
     description=models.CharField(max_length=200)
     is_done=models.BooleanField()
     user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
